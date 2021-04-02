362a90c6c92bf6a8b51692925f14c8a210615aab
Tom Rogers <tom@roguevendor.com>
2021-04-02 10:08:11 -0500

    (getters): impl & tests for getRoot

--- src/get-root.spec.js
+++ src/get-root.spec.js
@@ -1,1 +1,34 @@
-describe(`getRoot( directory, basename, sep )`, () => {})
+const getRoot = require('./get-root')
+
+
+describe(`getRoot( directory, basename, sep )`, () => {
+
+	it(`returns the empty string if directory does not begin with sep`, () => {
+		expect(
+			getRoot('path/to', 'some.fil', '/')
+		).toBe('')
+
+		expect(
+			getRoot('Documents and Settings\\tomprogers', 'some.fil', '\\')
+		).toBe('')
+	})
+
+	it(`returns the appropriate sep character if directory begins with one`, () => {
+		expect(
+			getRoot('/path/to', 'some.fil', '/')
+		).toBe('/')
+
+		expect(
+			getRoot('/', '', '/')
+		).toBe('/')
+
+		expect(
+			getRoot('\\Documents and Settings\\tomprogers', 'some.fil', '\\')
+		).toBe('\\')
+
+		expect(
+			getRoot('\\', '', '\\')
+		).toBe('\\')
+	})
+
+})
