8d51de0cfc11fd2902bba1a6cf9189b824c059e0
Tom Rogers <tom@roguevendor.com>
2021-04-02 11:50:02 -0500

    (getters): impl & tests of getFolder

--- src/get-folder.js
+++ src/get-folder.js
@@ -1,1 +1,8 @@
-module.exports = function getFolder( directory, basename, sep ) {}
+const getSegments = require('./get-segments')
+
+
+module.exports = function getFolder( directory, basename, sep ) {
+	let segments = getSegments(directory, basename, sep).reverse()
+	if(segments[0] === basename) segments.shift()
+	return segments[0] || ''
+}
