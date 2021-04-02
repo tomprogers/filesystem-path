088097fd9a85c170b0e0597b206fe53f4c5f91ff
Tom Rogers <3911927+tomprogers@users.noreply.github.com>
2021-04-02 11:17:38 -0500

    (getters): impl & tests for getSegments

--- src/get-segments.js
+++ src/get-segments.js
@@ -1,1 +1,12 @@
-module.exports = function getSegments( directory, basename, sep ) {}
+module.exports = function getSegments( directory, basename, sep ) {
+	let segments = []
+
+	if( directory ) {
+		if( directory.startsWith(sep) ) directory = directory.slice(1)
+		segments = segments.concat(directory.split(sep))
+	}
+
+	if(basename) segments.push(basename)
+
+	return segments
+}
