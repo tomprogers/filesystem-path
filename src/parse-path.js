b82a46a453ab7f0d790f44843e74219989598491
Tom Rogers <3911927+tomprogers@users.noreply.github.com>
2021-04-02 03:55:36 -0500

    (parse-path): rewrote parse-path to normalize 3-part signature

--- src/parse-path.js
+++ src/parse-path.js
@@ -1,26 +1,59 @@
-module.exports = function parsePath( path, sep = '/' ) {
-	// TODO: rewrite to use lastIndexOf, after you have comprehensive unit tests
-	let [ basename, ...parts ] = path.split(sep).reverse()
-	let directory = parts.slice().reverse().join(sep)
+const sep_nix = '/'
+const sep_win = '\\'
+const dot = '.'
 
-	let root = ''
-	if(directory.charAt(0) === sep) root = sep
-	let _isAbsolute = root === sep
-	let folder = parts.slice().reverse().pop()
+module.exports = function parsePath( directory, basename, sep ) {
+	if( typeof directory !== 'string' ) throw new TypeError('directory must be a string')
+	if( !['undefined', 'string'].includes(typeof basename) ) throw new TypeError('basename must be a string or omitted')
+	if( !['undefined', 'string'].includes(typeof sep) ) throw new TypeError('sep must be a string or omitted')
 
-	let [ ext, ...fnameParts ] = basename.split('.').reverse()
-	let filename = fnameParts.reverse().join('.')
+	// shift args around in case basename was omitted
 
-	let segments = parts.slice().reverse().concat(filename).filter(s => s.trim() !== '')
+	let basenameIsASep = basename === sep_nix || basename === sep_win
+	if( basenameIsASep && sep === undefined ) {
+		sep = basename
+		basename = ''
+	}
+
+	// now that args have been aligned across different calling patterns, provide defaults
+
+	if( !basename ) basename = ''
+	if( !sep ) sep = sep_nix
+
+	if( basename.includes(sep) ) throw new RangeError('basename must not contain directory separator')
+
+	// now that args are straightened-out, normalize inputs
+
+	// condense any beginning seps
+	let rooted = false
+	while( directory.beginsWith(sep) ) {
+		rooted = true
+		directory = directory.slice(1)
+	}
+	if(rooted) directory = sep + directory
+
+	// remove trailing seps
+	let trailed = false
+	while( directory.endsWith(sep) ) {
+		trailed = true
+		directory = directory.slice(0, -1)
+	}
+
+	// if filename wasn't provided, and directory arg's final seg has a dotted suffix, treat final seg as the filename
+	if( !basename && !trailed ) {
+		let lastDotIdx = directory.lastIndexOf(dot)
+		let lastSepIdx = directory.lastIndexOf(sep)
+		let finalSegmentHasExt = lastDotIdx > lastSepIdx && lastDotIdx < directory.length - 1
+
+		if( finalSegmentHasExt ) {
+			basename = directory.slice(lastSepIdx + 1)
+			directory = directory.slice(0, lastSepIdx)
+		}
+	}
 
 	return {
-		root,
 		directory,
-		folder,
 		basename,
-		filename,
-		ext,
-		segments,
-		_isAbsolute
+		sep
 	}
 }
