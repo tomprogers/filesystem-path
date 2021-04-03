f79c4b03094933ea77ed05c8632d8e207478e0e0
Tom Rogers <3911927+tomprogers@users.noreply.github.com>
2021-04-02 19:28:47 -0500

    (class): wired up instance accessors

--- src/filesystem-path.js
+++ src/filesystem-path.js
@@ -1,4 +1,9 @@
 const parseDFS = require('./parse-dfs')
+const _getRoot = require('./get-root')
+const _getFolder = require('./get-folder')
+const _getBasename = require('./get-basename')
+const _getExt = require('./get-ext')
+const _getSegments = require('./get-segments')
 
 
 module.exports = class FilesystemPath {
@@ -7,4 +12,17 @@ module.exports = class FilesystemPath {
 		Object.assign(this, parseDFS(directory, filename, sep))
 	}
 
+	getRoot =      () => _getRoot(..._getCoreProps(this))
+	getDirectory = () => this.directory
+	getFolder =    () => _getFolder(..._getCoreProps(this))
+	getBasename =  () => _getBasename(..._getCoreProps(this))
+	getFilename =  () => this.filename
+	getExt =       () => _getExt(..._getCoreProps(this))
+	getSep =       () => this.sep
+	getSegments =  () => _getSegments(..._getCoreProps(this))
+
+}
+
+function _getCoreProps({ directory, filename, sep }) {
+	return [ directory, filename, sep ]
 }
