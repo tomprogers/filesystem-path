3a2f4fdcbe9dc3cdd552fe3a9722983d46941d4b
Tom Rogers <3911927+tomprogers@users.noreply.github.com>
2021-04-02 23:17:45 -0500

    (ext): getExt now includes dot in non-empty returns

--- src/get-ext.js
+++ src/get-ext.js
@@ -3,6 +3,6 @@ const dot = '.'
 module.exports = function getExt( directory, filename, sep ) {
 	let lastDotIdx = filename.lastIndexOf(dot)
 
-	if( lastDotIdx > -1 && lastDotIdx < filename.length - 1 ) return filename.slice(lastDotIdx + 1)
+	if( lastDotIdx > -1 ) return filename.slice(lastDotIdx)
 	return ''
 }
--- src/get-ext.spec.js
+++ src/get-ext.spec.js
@@ -4,13 +4,13 @@ const getExt = require('./get-ext')
 describe(`getExt( directory, filename, sep )`, () => {
 
 	it(`handles the typical case`, () => {
-		expect(getExt(null, 'file.ext')).toBe('ext')
-		expect(getExt(null, 'multi.dot.name')).toBe('name')
-		expect(getExt(null, 'f.e')).toBe('e')
+		expect(getExt(null, 'file.ext')).toBe('.ext')
+		expect(getExt(null, 'multi.dot.name')).toBe('.name')
+		expect(getExt(null, 'f.e')).toBe('.e')
 	})
 
 	it(`handles dotfiles`, () => {
-		expect(getExt(null, '.dotfile')).toBe('dotfile')
+		expect(getExt(null, '.dotfile')).toBe('.dotfile')
 	})
 
 	it(`handles files with no ext`, () => {
@@ -18,7 +18,7 @@ describe(`getExt( directory, filename, sep )`, () => {
 	})
 
 	it(`handles dumb hypotheticals`, () => {
-		expect(getExt(null, 'sentence.')).toBe('')
+		expect(getExt(null, 'sentence.')).toBe('.')
 	})
 
 })
