2d52e7e211c662f16631e8ae266c4d0aee751234
Tom Rogers <3911927+tomprogers@users.noreply.github.com>
2021-04-04 17:39:35 -0500

    (class): minor restructuring away from _monadicAssign

--- src/filesystem-path.js
+++ src/filesystem-path.js
@@ -1,10 +1,10 @@
 const parseDFS = require('./parse-dfs')
-const _getRoot = require('./get-root')
-const _getFolders = require('./get-folders')
 const _getBasename = require('./get-basename')
 const _getExt = require('./get-ext')
-const _getSegments = require('./get-segments')
+const _getFolders = require('./get-folders')
 const _getPath = require('./get-path')
+const _getRoot = require('./get-root')
+const _getSegments = require('./get-segments')
 const _setBasename = require('./set-basename')
 const _setDirectory = require('./set-directory')
 const _setExt = require('./set-ext')
@@ -30,16 +30,16 @@ module.exports = class FilesystemPath {
 	getSegments  = () => _getSegments(..._getCoreProps(this))
 	getSep       = () => this.sep
 
-	toString     = () => _getPath(..._getCoreProps(this))
+	setBasename  = ( newBasename  ) => _monad(this, _setBasename,  newBasename )
+	setDirectory = ( newDirectory ) => _monad(this, _setDirectory, newDirectory)
+	setExt       = ( newExt       ) => _monad(this, _setExt,       newExt      )
+	setFilename  = ( newFilename  ) => _monad(this, _setFilename,  newFilename )
+	setFolders   = ( newFolders   ) => _monad(this, _setFolders,   newFolders  )
+	setRoot      = ( newRoot      ) => _monad(this, _setRoot,      newRoot     )
+	setSegments  = ( newSegments  ) => _monad(this, _setSegments,  newSegments )
+	setSep       = ( newSep       ) => _monad(this, _setSep,       newSep      )
 
-	setBasename  = ( newBasename  ) => _monadicAssign(this,  _setBasename(..._getCoreProps(this), newBasename ))
-	setDirectory = ( newDirectory ) => _monadicAssign(this, _setDirectory(..._getCoreProps(this), newDirectory))
-	setExt       = ( newExt       ) => _monadicAssign(this,       _setExt(..._getCoreProps(this), newExt      ))
-	setFilename  = ( newFilename  ) => _monadicAssign(this,  _setFilename(..._getCoreProps(this), newFilename ))
-	setFolders   = ( newFolders   ) => _monadicAssign(this,   _setFolders(..._getCoreProps(this), newFolders  ))
-	setRoot      = ( newRoot      ) => _monadicAssign(this,      _setRoot(..._getCoreProps(this), newRoot     ))
-	setSegments  = ( newSegments  ) => _monadicAssign(this,  _setSegments(..._getCoreProps(this), newSegments ))
-	setSep       = ( newSep       ) => _monadicAssign(this,       _setSep(..._getCoreProps(this), newSep      ))
+	toString     = () => _getPath(..._getCoreProps(this))
 
 }
 
@@ -49,7 +49,10 @@ function _getCoreProps({ directory, filename, sep }) {
 }
 
 
-function _monadicAssign( monad, patch ) {
-	Object.assign(monad, patch)
-	return monad
+function _monad( self, func, ...args ) {
+	return Object.assign(
+		self,
+		func(..._getCoreProps(self),
+		...args)
+	)
 }
