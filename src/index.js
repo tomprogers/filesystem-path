07e088cdccc7b0468fee134783e9c597a50caf22
Tom Rogers <tom@roguevendor.com>
2021-04-04 14:50:04 -0500

    (module): set up module exports, based on UMD's returnExports recipe, to support node & browsers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 const FilesystemPath = require('./filesystem-path')
 
-(function (factory) {
-	if (typeof define === 'function' && define.amd) {
-		define([], factory)
-	} else if (typeof module === 'object' && module.exports) {
-		module.exports = factory()
-	}
-}(function () {
-	// Just return a value to define the module export.
-	// This example returns an object, but the module
-	// can return a function as the exported value.
-	return FilesystemPath
-}))
+
+if( typeof define === 'function' && define.amd ) {
+	define([], () => FilesystemPath)
+
+} else if( typeof module === 'object' && module.exports ) {
+	module.exports = FilesystemPath
+
+}
