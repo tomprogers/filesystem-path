da253f73822b36d002b26a07b53f11fee7bc1f46
Tom Rogers <3911927+tomprogers@users.noreply.github.com>
2021-04-08 20:58:55 -0500

    (getters): reworked getRoot to rely on folders

--- src/get-root.spec.js
+++ src/get-root.spec.js
@@ -1,34 +1,52 @@
 const getRoot = require('./get-root')
 
 
-describe(`getRoot( directory, filename, sep )`, () => {
+describe(`getRoot( absolute, folders, filename, sep )`, () => {
 
 	it(`returns the empty string if directory does not begin with sep`, () => {
-		expect(
-			getRoot('path/to', 'some.fil', '/')
-		).toBe('')
+		expect(getRoot(
+			false,
+			['path', 'to'],
+			'some.fil',
+			'/'
+		)).toBe('')
 
-		expect(
-			getRoot('Documents and Settings\\tomprogers', 'some.fil', '\\')
-		).toBe('')
+		expect(getRoot(
+			false,
+			['Documents and Settings', 'tomprogers'],
+			'some.fil',
+			'\\'
+		)).toBe('')
 	})
 
 	it(`returns the appropriate sep character if directory begins with one`, () => {
-		expect(
-			getRoot('/path/to', 'some.fil', '/')
-		).toBe('/')
+		expect(getRoot(
+			true,
+			['path', 'to'],
+			'some.fil',
+			'/'
+		)).toBe('/')
 
-		expect(
-			getRoot('/', '', '/')
-		).toBe('/')
+		expect(getRoot(
+			true,
+			[],
+			'',
+			'/'
+		)).toBe('/')
 
-		expect(
-			getRoot('\\Documents and Settings\\tomprogers', 'some.fil', '\\')
-		).toBe('\\')
+		expect(getRoot(
+			true,
+			['Documents and Settings', 'tomprogers'],
+			'some.fil',
+			'\\'
+		)).toBe('\\')
 
-		expect(
-			getRoot('\\', '', '\\')
-		).toBe('\\')
+		expect(getRoot(
+			true,
+			[],
+			'',
+			'\\'
+		)).toBe('\\')
 	})
 
 })
