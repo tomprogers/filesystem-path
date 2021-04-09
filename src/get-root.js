ed3b339d6d861091443c797e45480f10aa68dc5c
Tom Rogers <tom@roguevendor.com>
2021-04-08 20:58:55 -0500

    (getters): reworked getRoot to rely on folders

--- src/get-root.js
+++ src/get-root.js
@@ -1,3 +1,3 @@
-module.exports = function getRoot( directory, filename, sep ) {
-	return directory.charAt(0) === sep ? sep : ''
+module.exports = function getRoot( absolute, folders, filename, sep ) {
+	return absolute ? sep : ''
 }
