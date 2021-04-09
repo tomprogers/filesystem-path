7635e7c22952fccadd20fc62332de6c4b72ee77c
Tom Rogers <tom@roguevendor.com>
2021-04-08 20:53:48 -0500

    (getters): reworked getPath to rely on folders

--- src/get-path.js
+++ src/get-path.js
@@ -1,5 +1,8 @@
-module.exports = function getPath( directory, filename, sep ) {
-	let path = directory
+module.exports = function getPath( absolute, folders, filename, sep ) {
+	let path = ''
+
+	if( absolute ) path += sep
+	if( folders.length ) path += folders.join(sep)
 	if( path && filename && !path.endsWith(sep) ) path += sep
 	path += filename
 
