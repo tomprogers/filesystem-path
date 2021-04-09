87cfa9f42550a0b95c86672dbaaf056e3f53f45b
Tom Rogers <3911927+tomprogers@users.noreply.github.com>
2021-04-08 21:04:56 -0500

    (getters): reworked getExt to rely on folders

--- src/get-ext.js
+++ src/get-ext.js
@@ -1,6 +1,6 @@
 const dot = '.'
 
-module.exports = function getExt( directory, filename, sep ) {
+module.exports = function getExt( absolute, folders, filename, sep ) {
 	let lastDotIdx = filename.lastIndexOf(dot)
 
 	if( lastDotIdx > -1 ) return filename.slice(lastDotIdx)
