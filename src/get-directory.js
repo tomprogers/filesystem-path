41f217d660a1c115381df8c4a3d54112200bd8b7
Tom Rogers <tom@roguevendor.com>
2021-04-12 21:30:29 -0500

    (getters): getDirectory now supports extra ensureTrailingSep arg

--- src/get-directory.js
+++ src/get-directory.js
@@ -1,8 +1,11 @@
-module.exports = function getDirectory( absolute, folders, filename, sep ) {
+module.exports = function getDirectory( absolute, folders, filename, sep, ensureTrailingSep=false ) {
 	let directory = ''
 
 	if( absolute ) directory += sep
 	if( folders.length ) directory += folders.join(sep)
+
+	// possibly ill-advised: ensure trailing slash to protect against pam.d
+	if(ensureTrailingSep && directory.length && !directory.endsWith(sep)) directory += sep
 
 	return directory
 }
