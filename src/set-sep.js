38941d678fd44ade145c037220e3cb91295e3af1
Tom Rogers <3911927+tomprogers@users.noreply.github.com>
2021-04-13 21:16:28 -0500

    (setters): updated setSep to work with absolute & folders

--- src/set-sep.js
+++ src/set-sep.js
@@ -1,23 +1,29 @@
+const getDirectory = require('./get-directory')
 const parseDFS = require('./parse-dfs')
 
 
 const SEP_NIX = '/'
 const SEP_WIN = '\\'
 const PERMITTED_SEPS = [ SEP_NIX, SEP_WIN ]
+const R_SEP_NIX = /\//gi
+const R_SEP_WIN = /\\/gi
 
-module.exports = function setSep( directory, filename, sep, newSep ) {
+module.exports = function setSep( absolute, folders, filename, sep, newSep ) {
 	if( typeof newSep !== 'string' )
 		throw new TypeError('newSep must be a string')
 
 	if( !PERMITTED_SEPS.includes(newSep) )
 		throw new RangeError('newSep must be either backslash or forward-slash as a string')
 
-	// older nodejs lacks String.prototype.replaceAll, so using a regex to convert old seps
-	let escapedOldSep = sep.replace(/\\/, '\\\\') // backslashes need extra escaping as regex chars
-	let rOldSep = new RegExp(escapedOldSep, 'gi')
+	// older nodejs (< 14.6 ?) lacks String.prototype.replaceAll, so using a regex to convert old seps
+	let oldSepRegex = sep === SEP_NIX
+		? R_SEP_NIX
+		: R_SEP_WIN
+
+	let newDirectory = getDirectory(absolute, folders, filename, sep).replace(oldSepRegex, newSep)
 
 	return parseDFS(
-		directory.replace(rOldSep, newSep),
+		newDirectory,
 		filename,
 		newSep
 	)
