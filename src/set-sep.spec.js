40bb1a28d102857d1f343de2b2c101ee8afac655
Tom Rogers <tom@roguevendor.com>
2021-04-13 21:16:28 -0500

    (setters): updated setSep to work with absolute & folders

--- src/set-sep.spec.js
+++ src/set-sep.spec.js
@@ -1,33 +1,22 @@
 const setSep = require('./set-sep')
 
 
-describe(`setSep( directory, filename, sep, newSep`, () => {
+describe(`setSep( absolute, folders, filename, sep, newSep`, () => {
 
 	it(`changes the sep`, () => {
 		expect(
-			setSep('/Users/tomprogers/projects/filesystem-path', 'README.md', '/', '\\')
+			setSep(true, ['Users', 'tomprogers', 'projects', 'filesystem-path'], 'README.md', '/', '\\')
 		).toHaveProperty('sep', '\\')
 
 		expect(
-			setSep('\\Documents and Settings\\tomprogers\\projects\\filesystem-path', 'README.md', '\\', '/')
+			setSep(true, ['Documents and Settings', 'tomprogers', 'projects', 'filesystem-path'], 'README.md', '\\', '/')
 		).toHaveProperty('sep', '/')
-	})
-
-
-	it(`converts existing separators`, () => {
-		expect(
-			setSep('/Users/tomprogers/projects/filesystem-path', 'README.md', '/', '\\')
-		).toHaveProperty('directory', '\\Users\\tomprogers\\projects\\filesystem-path')
-
-		expect(
-			setSep('\\Documents and Settings\\tomprogers\\projects\\filesystem-path', 'README.md', '\\', '/')
-		).toHaveProperty('directory', '/Documents and Settings/tomprogers/projects/filesystem-path')
 	})
 
 
 	it(`throws TypeError if newSep not a string`, () => {
 		expect(() => {
-			setSep('/Users/tomprogers/projects/filesystem-path', 'README.md', '/', 5)
+			setSep(true, ['Users', 'tomprogers', 'projects', 'filesystem-path'], 'README.md', '/', 5)
 		}).toThrow(
 			TypeError
 		)
@@ -36,7 +25,7 @@ describe(`setSep( directory, filename, sep, newSep`, () => {
 
 	it(`throws RangeError if newSep not a valid sep`, () => {
 		expect(() => {
-			setSep('/Users/tomprogers/projects/filesystem-path', 'README.md', '/', '5')
+			setSep(true, ['Users', 'tomprogers', 'projects', 'filesystem-path'], 'README.md', '/', '5')
 		}).toThrow(
 			RangeError
 		)
