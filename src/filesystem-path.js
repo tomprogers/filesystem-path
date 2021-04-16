4f15ceff4f753fe569f342fcb18623e8083669c0
Tom Rogers <tom@roguevendor.com>
2021-04-15 20:28:15 -0500

    (class): updated class wiring to work with absolute & folders

--- src/filesystem-path.js
+++ src/filesystem-path.js
@@ -1,10 +1,11 @@
 const parseDFS = require('./parse-dfs')
 const _getBasename = require('./get-basename')
+const _getDirectory = require('./get-directory')
 const _getExt = require('./get-ext')
-const _getFolders = require('./get-folders')
 const _getPath = require('./get-path')
 const _getRoot = require('./get-root')
 const _getSegments = require('./get-segments')
+const _setAbsolute = require('./set-absolute')
 const _setBasename = require('./set-basename')
 const _setDirectory = require('./set-directory')
 const _setExt = require('./set-ext')
@@ -21,38 +22,39 @@ module.exports = class FilesystemPath {
 		Object.assign(this, parseDFS(directory, filename, sep))
 	}
 
-	getBasename  = () => _getBasename(..._getCoreProps(this))
-	getDirectory = () => this.directory
-	getExt       = () => _getExt(..._getCoreProps(this))
+	getAbsolute  = () => this.absolute
+	getBasename  = () => _getBasename (..._getCoreProps(this))
+	getDirectory = () => _getDirectory(..._getCoreProps(this))
+	getExt       = () => _getExt      (..._getCoreProps(this))
 	getFilename  = () => this.filename
-	getFolders   = () => _getFolders(..._getCoreProps(this))
-	getRoot      = () => _getRoot(..._getCoreProps(this))
-	getSegments  = () => _getSegments(..._getCoreProps(this))
+	getFolders   = () => this.folders
+	getRoot      = () => _getRoot     (..._getCoreProps(this))
+	getSegments  = () => _getSegments (..._getCoreProps(this))
 	getSep       = () => this.sep
 
-	setBasename  = ( newBasename  ) => _monad(this, _setBasename,  newBasename )
-	setDirectory = ( newDirectory ) => _monad(this, _setDirectory, newDirectory)
-	setExt       = ( newExt       ) => _monad(this, _setExt,       newExt      )
-	setFilename  = ( newFilename  ) => _monad(this, _setFilename,  newFilename )
-	setFolders   = ( newFolders   ) => _monad(this, _setFolders,   newFolders  )
-	setRoot      = ( newRoot      ) => _monad(this, _setRoot,      newRoot     )
-	setSegments  = ( newSegments  ) => _monad(this, _setSegments,  newSegments )
-	setSep       = ( newSep       ) => _monad(this, _setSep,       newSep      )
+	setAbsolute  = ( newAbsolute  ) => _mutateCoreProps(this, _setAbsolute,  newAbsolute )
+	setBasename  = ( newBasename  ) => _mutateCoreProps(this, _setBasename,  newBasename )
+	setDirectory = ( newDirectory ) => _mutateCoreProps(this, _setDirectory, newDirectory)
+	setExt       = ( newExt       ) => _mutateCoreProps(this, _setExt,       newExt      )
+	setFilename  = ( newFilename  ) => _mutateCoreProps(this, _setFilename,  newFilename )
+	setFolders   = ( newFolders   ) => _mutateCoreProps(this, _setFolders,   newFolders  )
+	setRoot      = ( newRoot      ) => _mutateCoreProps(this, _setRoot,      newRoot     )
+	setSegments  = ( newSegments  ) => _mutateCoreProps(this, _setSegments,  newSegments )
+	setSep       = ( newSep       ) => _mutateCoreProps(this, _setSep,       newSep      )
 
 	toString     = () => _getPath(..._getCoreProps(this))
 
 }
 
 
-function _getCoreProps({ directory, filename, sep }) {
-	return [ directory, filename, sep ]
+function _getCoreProps({ absolute, folders, filename, sep }) {
+	return [ absolute, folders, filename, sep ]
 }
 
 
-function _monad( self, func, ...args ) {
+function _mutateCoreProps( self, mutatorFn, ...mutatorArgs ) {
 	return Object.assign(
 		self,
-		func(..._getCoreProps(self),
-		...args)
+		mutatorFn(..._getCoreProps(self), ...mutatorArgs)
 	)
 }
